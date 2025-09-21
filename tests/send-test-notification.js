3473874fb065bcaf327537dd75724f4c9ae1557f
Christopher Allène <66381046+callms@users.noreply.github.com>
2025-09-21 15:33:42 +0200

    Fix/remote token on 410 (#3)
    * fix: add debug for removing old tokens from redis
    
    * fix: return results

--- tests/send-test-notification.js
+++ tests/send-test-notification.js
@@ -1,11 +1,14 @@
 const opengluckApn = require("..");
 
 (async () => {
-  await opengluckApn.sendNotification({
+  const result = await opengluckApn.sendNotification({
     alert: { title: "Test Notification", body: "This is a test notification" },
     contentAvailable: true,
     sound: "default",
     category: "LOW",
   });
+  for (const line of result.debugFailed) {
+    console.log(line);
+  }
   process.exit();
 })();
