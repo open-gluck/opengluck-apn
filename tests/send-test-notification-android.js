13e9c9e6e30343a62ed77a0364f54546092a1174
Christopher Allène <christopher.allene@mobsuccess.com>
2025-12-13 09:52:53 +0000

    fix: test android

--- tests/send-test-notification-android.js
+++ tests/send-test-notification-android.js
@@ -1,14 +1,23 @@
 const opengluckApn = require("..");
 
 (async () => {
-  const time = new Date().toLocaleTimeString("en-GB", { hour: "2-digit", minute: "2-digit" });
-  const result = await opengluckApn.sendNotification({
-    alert: { title: `Test Notification ${time}`, body: "This is a test notification" },
-    contentAvailable: true,
-    sound: "default",
+  const time = new Date().toLocaleTimeString("en-GB", {
+    hour: "2-digit",
+    minute: "2-digit",
   });
-  for (const line of result.debugFailed) {
-    console.log(line);
+  for (const app of ["Android", "Android.production"]) {
+    const result = await opengluckApn.sendNotification({
+      app,
+      alert: {
+        title: `Test Notification ${time}`,
+        body: "This is a test notification",
+      },
+      contentAvailable: true,
+      sound: "default",
+    });
+    for (const line of result.debugFailed) {
+      console.log(line);
+    }
   }
   process.exit();
 })();
