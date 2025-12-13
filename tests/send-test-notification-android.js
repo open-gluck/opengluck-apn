7b7fd602be48a612f20cb3f8911ebfe2958266fe
Christopher Allène <christopher.allene@mobsuccess.com>
2025-12-13 09:51:41 +0000

    fix: remove dev restriction for Android test

--- tests/send-test-notification-android.js
+++ tests/send-test-notification-android.js
@@ -3,7 +3,6 @@ const opengluckApn = require("..");
 (async () => {
   const time = new Date().toLocaleTimeString("en-GB", { hour: "2-digit", minute: "2-digit" });
   const result = await opengluckApn.sendNotification({
-    app: "Android",
     alert: { title: `Test Notification ${time}`, body: "This is a test notification" },
     contentAvailable: true,
     sound: "default",
